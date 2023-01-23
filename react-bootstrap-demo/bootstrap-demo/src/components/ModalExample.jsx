115f435f98de2ab38555a63425be30115d1f1119
Aparna Samudrala <67942273+AparnaSamudrala@users.noreply.github.com>
2023-01-23 17:50:37 +0530

    Add files via upload

--- react-bootstrap-demo/bootstrap-demo/src/components/ModalExample.jsx
+++ react-bootstrap-demo/bootstrap-demo/src/components/ModalExample.jsx
@@ -2,33 +2,58 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function ModalExample() {
-  const [show, setShow] = useState(false);
+function ModalExample({EmpArr}) {
+  /* const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => setShow(true); */
+  const [show, setShow] = useState(false);
+  const [data,setData] = useState({});
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+    <h3>Displaying Employee Details in Modal</h3>
+    <hr/>
+  {
+      
+      EmpArr.map((i) => (
+        
+      
+        <Button key={i.id} className="me-3" variant="primary" onClick={() => {setShow(true);setData(i);}}>
+        {i.name}
+        </Button>
+        
+        
+      ))
+  }
+      {console.log(data)}
+      <Modal show={show}   onHide={() => setShow(false)}>
         <Modal.Header>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Info about:  {data.name}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+        <ul>
+              <li> Employee ID : {data.id}</li>
+              <li> Employee Name: {data.name}</li>
+              <li>Employee Address: {data.address}</li>
+              <li>Email :{data.email}</li>
+              <li>Phone: {data.phone}</li>
+            </ul>
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={() => setShow(false)}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={() => setShow(false)}>
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
+
+      
+      
+
     </>
-  );
+  )
 }
 export default ModalExample;
